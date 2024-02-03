1010f15fc8663af85f07a222ebb62a26d5fc3a85
Vinicius2021 <Viniciusoliveira2018@hotmail.com>
2024-02-03 20:06:57 -0300

    import InputMask

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -289,10 +289,20 @@
     </div>
 
     @yield('scripts')
+
+
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <script src="/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js"></script>
     <script src="/js/dashboard.js"></script>
     <script src="/js/color-modes.js"></script>
+    {{-- InputMask --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
+
+    {{-- BLocUI loading--}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.js"></script>
+
+    <script src="/js/projeto.js"></script>
 
 </body>
 
